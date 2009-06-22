c0b04df8fb95dcdf788d241f053a3dd44c369d4a
John Flatness <john@zerocrates.org>
2009-06-22 20:37:46 +0000

    Adds function to determine the width of PDF strings.

--- libraries/Reports/ReportGenerator/PdfQrCode.php
+++ libraries/Reports/ReportGenerator/PdfQrCode.php
@@ -99,6 +99,35 @@ class Reports_ReportGenerator_PdfQrCode extends Reports_ReportGenerator
         $pdf->save($filename);
     }
     
+    /**
+    * Returns the total width in points of the string using the specified font         
+    * and size.
+    *
+    * This is not the most efficient way to perform this calculation. I'm
+    * concentrating optimization efforts on the upcoming layout manager class.
+    * Similar calculations exist inside the layout manager class, but widths
+    * are generally calculated only after determining line fragments.
+    *
+    * @param string $string
+    * @param Zend_Pdf_Resource_Font $font
+    * @param float $fontSize Font size in points
+    * @return float
+    */
+    private function widthForStringUsingFontSize($string, $font, $fontSize)
+    {
+        $drawingString = iconv('UTF-8', 'UTF-16BE//IGNORE', $string);
+        $characters = array();
+        for ($i = 0; $i < strlen($drawingString); $i++) {
+            $characters[] = (ord($drawingString[$i++]) << 8) | 
+                             ord($drawingString[$i]);
+        }
+        $glyphs = $font->cmap->glyphNumbersForCharacters($characters);
+        $widths = $font->widthsForGlyphs($glyphs);
+        $stringWidth = (array_sum($widths) / $font->getUnitsPerEm()) * $fontSize;
+        return $stringWidth;
+    }
+    
+    
     public function getReadableName() {
         return 'QR Code (PDF)';
     }
