3aa56eb302b11820ae13647a48a9fed574d0746c
Jeremy Boggs <jeremy@clioweb.org>
2009-06-22 17:07:57 +0000

    Updates screen styles for Reports HTML output.

--- libraries/Reports/ReportGenerator/HTML.php
+++ libraries/Reports/ReportGenerator/HTML.php
@@ -19,35 +19,47 @@ class Reports_ReportGenerator_HTML extends Reports_ReportGenerator
     }
     
     private function outputHTML() { 
+        $reportName = $this->_report->name;
+        $reportDescription = $this->_report->description;
         ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <title>Omeka Report</title>
-<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />    
+<title>Omeka Report</title>
+<style type="text/css" media="screen">
+    body {background: #ccc; padding:0; margin:0;}
+    #report {width: 920px; margin: 0 auto; padding: 20px; background:#fff;}
+    .item {border-bottom: 1px solid #ccc;}
+    table {border-bottom:1px dotted #ccc; width:100%;}
+    th, td {vertical-align:top; padding: 10px 0;}
+    th {text-align:right; font-weight:bold;}
+</style>
+</head>
 <body>
-<h1>Omeka Report</h1>
-<table>
-<tr>
-<td>Generated on <?php echo date('r') ?></td>
-</tr>
-</table>
-<?php foreach($this->_items as $item) : ?>
-<h2>Item <?php echo $item->id; ?></h2>
-<?php     foreach($item->getAllElementsBySet() as $set => $elements) : ?>
-<h3><?php echo $set; ?></h3>
-<table>
-<?php         foreach($elements as $element) : 
-                  foreach($item->getTextsByElement($element) as $text) : ?>
-<tr>
-<td><strong><?php echo $element->name; ?></strong></td>
-<td><?php echo $text->text; ?></td>
-</tr>
-<?php             endforeach; ?>
-<?php         endforeach; ?>
-</table>
-<?php     endforeach; ?>
-<?php endforeach; ?>
+    <div id="report">
+        <h1><?php echo $reportName; ?></h1>
+        <p>Generated on <?php echo date('r') ?></p>
+        <?php echo $reportDescription; ?>
+        <?php foreach($this->_items as $item) : ?>
+            <div class="item" id="item-<?php echo $item->id; ?>">
+                <h2>Item <?php echo $item->id; ?></h2>
+                <?php foreach($item->getAllElementsBySet() as $set => $elements) : ?>
+                <h3><?php echo $set; ?></h3>
+                <table class="element-texts" cellpadding="0" cellspacing="0">
+                <?php foreach($elements as $element) : ?>
+                    <?php foreach($item->getTextsByElement($element) as $text) : ?>
+                    <tr class="element">
+                        <th scope="row" class="element-name"><?php echo $element->name; ?></th>
+                        <td class="element-value"><?php echo $text->text; ?></td>
+                    </tr>
+                    <?php endforeach; ?>
+                <?php endforeach; ?>
+                </table>
+                <?php endforeach; ?>
+                </div>
+        <?php endforeach; ?>
+    </div>
 </body>
 </html>
 <?php
