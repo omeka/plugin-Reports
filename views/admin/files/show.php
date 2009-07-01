a6544a24839f5b19a1644b66fb47145716c5d566
John Flatness <john@zerocrates.org>
2009-07-01 01:18:16 +0000

    Fixes error on output of files larger than the memory limit.

--- views/admin/files/show.php
+++ views/admin/files/show.php
@@ -9,10 +9,11 @@
  * @copyright Copyright (c) 2009 Center for History and New Media
  * @license http://www.gnu.org/licenses/gpl-3.0.txt
  */
-
 $generator = $reportsfile->getGenerator();
 
 header("Content-Type: {$generator->getContentType()}");
 header("Content-Disposition: inline; filename=report.{$generator->getExtension()}");
 
+//Stop output buffering to allow the output of large file
+ob_end_flush();
 readfile(REPORTS_SAVE_DIRECTORY . DIRECTORY_SEPARATOR . $reportsfile->filename);
