7c651056885444b81b52bcc42dccb2a4a69445bc
Patrick Murray-John <patrickmjchnm@gmail.com>
2011-08-31 14:52:01 -0400

    Tested and works using S3. Sets one week magic number for expiration

--- models/Reports/GenerateJob.php
+++ models/Reports/GenerateJob.php
@@ -8,10 +8,25 @@ class Reports_GenerateJob extends Omeka_JobAbstract
             ini_set('memory_limit', $memoryLimit);
             _log("Set memory limit to $memoryLimit");
         }
+        //@TODO: figure out this magic when I move to testing on a system that actually has S3.
+        $this->modifyS3Expiration(10080); //expires in one week
         $fileId = $this->_options['fileId'];
         $report = $this->_db->getTable('Reports_File')->find($fileId);
         $generator = $report->getGenerator();
         $generator->generate();
         $report->forceSave();
+    }
+    
+    public function modifyS3Expiration($minutes)
+    {
+        $context = Omeka_Context::getInstance();
+        $storageOptions = $context->getConfig('basic')->storage;
+        if(isset($storageOptions) ) {
+            $storageOptions = $storageOptions->toArray();
+            $storageOptions['adapterOptions']['expiration'] = $minutes;
+            $storage = new Omeka_Storage($storageOptions);
+            Zend_Registry::set('storage', $storage);
+            $context->setStorage($storage);
+        }
     }
 }
